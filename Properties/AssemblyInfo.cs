582a6578f07255389b3a196e71ef059a7c60f30e
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-09-15 09:42:11 +0100

    Adding google fake provider and Fake Build target

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CallWall.Web.GoogleProvider")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © CallWall 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
